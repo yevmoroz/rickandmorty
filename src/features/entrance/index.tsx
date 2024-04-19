2ebce3984efdc2e08c2a719efa5001e676d863e0
Yev Moroz <yev.moroz.dev@gmail.com>
2024-04-18 21:26:55 -0400

    welcome rickandmorty

--- src/features/entrance/index.tsx
+++ src/features/entrance/index.tsx
@@ -4,7 +4,9 @@ import { AppContainer } from '../app-container';
 import { Button } from '../button';
 
 const onPressStart = (): void => {
-  Alert.alert('Hold your horses, start small and take your time');
+  Alert.alert(
+    "Hold your horses, start small and take your time, son we'll have rick and morty library"
+  );
 };
 
 export const Entrance: React.FC = () => {
